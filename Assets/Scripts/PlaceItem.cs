f49e1cebdabe93783cf254db87d17fb18ed007f1
Alton Sturgis <asturgis@berkeley.edu>
2020-11-14 21:31:09 -0800

    yarn

---
+++ Assets/Scripts/PlaceItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceItem : MonoBehaviour
+{
+    //TODO
+    //Throw prefab item from the camera origin to playspace.
+}
