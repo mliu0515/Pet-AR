ef1e8b9761f7ee59841733ff883c5d5aa8c54325
May Liu <chonghualiu@berkeley.edu>
2020-11-15 16:11:50 -0800

    Placement indicator, place object on a plane.

---
+++ Assets/Scripts/TapToPlaceObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+//using UnityEngine.Experimental.XR
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+//So yea this script is used for placing item onto the scene.
+//So like food and stuff that is going to be thrown to the pet. 
+public class TapToPlaceObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+		{
+            placementIndicator.SetActive(false);
+		}
+	}
+
+    private void UpdatePlacementPose()
+	{
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneEstimated);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+		{
+            PlacementPose = hits[0].pose;
+		}
+	}
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, PlacementPose.position, PlacementPose.rotation);
+    }
+}
