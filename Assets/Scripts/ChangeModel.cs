c913a5e7798e71ff47e28ee9591ea892f7db3805
Alton Sturgis <asturgis@berkeley.edu>
2020-12-04 16:45:34 -0800

    fixes

--- Assets/Scripts/ChangeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ChangeModel : MonoBehaviour
-{
-    // TODO
-    //Use this script to call the PlaceOnPlane when a new model is chosen from the menu
-}
