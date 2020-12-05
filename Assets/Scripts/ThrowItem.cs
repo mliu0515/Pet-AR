1e974a1580197fbc909eaec9bc2d4b7fe5a2b896
Alton Sturgis <asturgis@berkeley.edu>
2020-12-04 17:38:40 -0800

    basic placement mechanics

---
+++ Assets/Scripts/ThrowItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowItem : MonoBehaviour
+{
+    //Throw prefab item from the camera origin to playspace.
+    public GameObject item;
+    public AudioSource throwSound;
+    int inventory = 5;
+    public void Throw()
+    {
+            if (GameObject.FindGameObjectsWithTag("Pet") != null)
+            {
+                Debug.Log("thrown" + item.name);
+                throwSound.Play();
+                if (inventory > 1 && GameObject.FindGameObjectWithTag("Pet") != null)
+                {
+                    Instantiate(item, transform.position + transform.forward, Quaternion.identity);
+                }
+            }
+        
+    }
+}
